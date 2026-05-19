cae161901ebeee5fd76cbdb528e68ddcc240d4d6
222112195 <222112195@stis.ac.id>
2026-05-19 10:33:45 +0800

    add cakupan usaha section

---
+++ resources/views/components/sections/cakupan-kbli.blade.php
@@ -0,0 +1,217 @@
+<section class="kbli-section" id="cakupan-kbli">
+    <div class="kbli-section__container">
+        <div class="kbli-section__header">
+            <span class="kbli-section__eyebrow">KBLI 2025</span>
+            <h2 class="kbli-section__title">Cakupan Lapangan Usaha</h2>
+            <p class="kbli-section__description">
+                Klasifikasi Baku Lapangan Usaha Indonesia (KBLI) 2025 digunakan untuk mengklasifikasikan kegiatan ekonomi dalam Sensus Ekonomi 2026.
+            </p>
+        </div>
+
+        <div class="kbli-grid">
+            <div class="kbli-column">
+                <div class="kbli-category-card" data-category="A">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/agriculture.png') }}" alt="Pertanian Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">A</span>
+                        <span class="kbli-category-card__name">Pertanian, Kehutanan, dan Perikanan</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="B">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/overmining.png') }}" alt="Pertambangan Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">B</span>
+                        <span class="kbli-category-card__name">Pertambangan dan Penggalian</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="C">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/oil-industry.png') }}" alt="Industri Pengolahan Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">C</span>
+                        <span class="kbli-category-card__name">Industri Pengolahan</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="D">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/electric-tower.png') }}" alt="">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">D</span>
+                        <span class="kbli-category-card__name">Pengadaan Listrik, Gas, Uap/Air Panas dan Udara Dingin</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="E">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/water-reuse.png') }}" alt="Penyediaan Air Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">E</span>
+                        <span class="kbli-category-card__name">Penyediaan Air, Pengelolaan Air Limbah, Penanganan Limbah, dan Remediasi</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="F">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/engineering.png') }}" alt="Konstruksi Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">F</span>
+                        <span class="kbli-category-card__name">Konstruksi</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="G">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/market.png') }}" alt="Perdagangan Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">G</span>
+                        <span class="kbli-category-card__name">Perdagangan Besar dan Eceran</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="H">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/transportation.png') }}" alt="Transportasi Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">H</span>
+                        <span class="kbli-category-card__name">Transportasi dan Penyimpanan</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="I">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/bibimbap.png') }}" alt="Penyediaan Akomodasi Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">I</span>
+                        <span class="kbli-category-card__name">Penyediaan Akomodasi dan Penyediaan Makan dan Minum</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="J">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/signal.png') }}" alt="Aktivitas Penerbitan Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">J</span>
+                        <span class="kbli-category-card__name">Aktivitas Penerbitan, Penyiaran, Produksi dan Distribusi Konten</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="kbli-column">
+                <div class="kbli-category-card" data-category="K">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/software.png') }}" alt="Aktivitas Telekomunikasi Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">K</span>
+                        <span class="kbli-category-card__name">Aktivitas Telekomunikasi, Pemrograman Komputer, Konsultansi, Infrastruktur Komputasi, dan Jasa Informasi Lainnya</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="L">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/money.png') }}" alt="Aktivitas Keuangan Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">L</span>
+                        <span class="kbli-category-card__name">Aktivitas Keuangan dan Asuransi</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="M">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/estate-agent.png') }}" alt="Real Estat Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">M</span>
+                        <span class="kbli-category-card__name">Real Estat</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="N">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/professionals.png') }}" alt="Aktivitas Profesional Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">N</span>
+                        <span class="kbli-category-card__name">Aktivitas Profesional, Ilmiah, dan Teknis</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="O">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/writer.png') }}" alt="Aktivitas Administratif Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">O</span>
+                        <span class="kbli-category-card__name">Aktivitas Administratif dan Penunjang Usaha</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="Q">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/notebook.png') }}" alt="Pendidikan Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">Q</span>
+                        <span class="kbli-category-card__name">Pendidikan</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="R">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/medical-team.png') }}" alt="Aktivitas Kesehatan Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">R</span>
+                        <span class="kbli-category-card__name">Aktivitas Kesehatan Manusia dan Aktivitas Sosial</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="S">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/paint-palette.png') }}" alt="Kesenian Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">S</span>
+                        <span class="kbli-category-card__name">Kesenian, Hiburan, dan Rekreasi</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="T">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/ellipse.png') }}" alt="Jasa Lainnya Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">T</span>
+                        <span class="kbli-category-card__name">Aktivitas Jasa Lainnya</span>
+                    </div>
+                </div>
+
+                <div class="kbli-category-card" data-category="V">
+                    <div class="kbli-category-card__icon">
+                        <img src="{{ asset('assets/img/worldwide-shipping.png') }}" alt="International Icon">
+                    </div>
+                    <div class="kbli-category-card__content">
+                        <span class="kbli-category-card__code">V</span>
+                        <span class="kbli-category-card__name">Aktivitas Badan Internasional dan Badan Ekstra Internasional Lainnya</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,5 +9,6 @@
     />
     <x-sections.map-usaha :usahaGmaps="$usahaGmaps" :statsGmaps="$statsGmaps" :allUsahaGmaps="$allUsahaGmaps" :kategoriOptions="$kategoriOptions" />
     <x-sections.tujuan-se />
+    <x-sections.cakupan-kbli />
     <x-partials.footer />
 </x-layouts.app>
