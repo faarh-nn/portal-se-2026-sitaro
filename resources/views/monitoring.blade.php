67de100303869015e634140975250d0dadcb8fae
222112195 <222112195@stis.ac.id>
2026-06-16 00:28:35 +0800

    add grid background

--- resources/views/monitoring.blade.php
+++ resources/views/monitoring.blade.php
@@ -1,6 +1,11 @@
 <x-layouts.app>
     <x-partials.navbar />
 
+    <div class="monitoring-section__container">
+        <x-sections.progress-pcl :pclData="$pclData" :pclTotals="$pclTotals" :pclDataPaginated="$pclDataPaginated" :pmlList="$pmlList" :lastUpdate="$lastUpdate" />
+        <x-sections.progress-pml :pmlData="$pmlData" :pmlTotals="$pmlTotals" :lastUpdate="$lastUpdate" />
+    </div>
+
     <x-sections.map-kecamatan
         :progressData="$progressData"
         :overallProgress="$overallProgress"
@@ -10,8 +15,6 @@
 
     <div class="monitoring-section__container">
         <x-sections.table-kecamatan :progressData="$progressData" :lastUpdate="$lastUpdate" />
-        <x-sections.progress-pcl :pclData="$pclData" :pclTotals="$pclTotals" :pclDataPaginated="$pclDataPaginated" :pmlList="$pmlList" :lastUpdate="$lastUpdate" />
-        <x-sections.progress-pml :pmlData="$pmlData" :pmlTotals="$pmlTotals" :lastUpdate="$lastUpdate" />
     </div>
 
     <x-partials.footer />
